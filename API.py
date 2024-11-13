fae5a55d056492d6e505ba0e5fcafe1da2b79202
arwaalkhathlan <arwa124344@gmail.com>
2024-11-13 14:32:31 +0300

    arwa alkhathlan

--- API.py
+++ API.py
@@ -27,34 +27,26 @@ class InputFeatures(BaseModel):
     Options: str
 
 def preprocessing(input_features: InputFeatures):
-    try:
-        dict_f = {
-            'Year': input_features.Year,
-            'Engine_Size': input_features.Engine_Size,
-            'Mileage': input_features.Mileage,
-            'Type_Accent': input_features.Type == 'Accent',
-            'Type_Land Cruiser': input_features.Type == 'Land Cruiser',
-            'Make_Hyundai': input_features.Make == 'Hyundai',
-            'Make_Mercedes': input_features.Make == 'Mercedes',
-            'Options_Full': input_features.Options == 'Full',
-            'Options_Standard': input_features.Options == 'Standard'
-        }
-
-        # Convert dict to a sorted list
-        features_list = [dict_f[key] for key in sorted(dict_f)]
-        scaled_features = scaler.transform([features_list])
-
-        return scaled_features
-    except Exception as e:
-        logger.error(f"Error in preprocessing: {e}")
-        raise HTTPException(status_code=400, detail="Bad Request: Preprocessing Failed")
+    dict_f = {
+    'Year': input_features.Year,
+    'Engine_Size': input_features.Engine_Size,
+    'Mileage': input_features.Mileage,
+    'Type_Accent': input_features.Type == 'Accent',
+    'Type_Land Cruiser': input_features.Type == 'LandCruiser',
+    'Make_Hyundai': input_features.Make == 'Hyundai',
+    'Make_Mercedes': input_features.Make == 'Mercedes',
+    'Options_Full': input_features.Options == 'Full',
+    'Options_Standard': input_features.Options == 'Standard'
+}
+# Convert dictionary values to a list in the correct order
+    features_list = [dict_f[key] for key in sorted(dict_f)]
+# Scale the input features
+    scaled_features = scaler.transform([list(dict_f.values
+    ())])
+    return scaled_features
 
 @app.post("/predict")
 async def predict(input_features: InputFeatures):
-    try:
-        data = preprocessing(input_features)
-        y_pred = model.predict(data)
-        return {"pred": y_pred.tolist()[0]}
-    except Exception as e:
-        logger.error(f"Prediction error: {e}")
-        raise HTTPException(status_code=500, detail="Internal Server Error: Prediction Failed")
+    data = preprocessing(input_features)
+    y_pred = model.predict(data)
+    return {"pred": y_pred.tolist()[0]}
