3d3893cd85d0010fa0ccf0c751eacd61ea3b9ae8
arwaalkhathlan <arwa124344@gmail.com>
2024-11-13 14:51:41 +0300

    arwa alkhathlan

--- Car_prices.py
+++ Car_prices.py
@@ -8,39 +8,34 @@ df = pd.read_csv('data/data_saudi_used_cars.csv')
 
 st.title("Car Price Prediction")
 
-# take the user input and saves it
-year = st.number_input("Year", min_value=1990, max_value=2025, value=2020)
+# Input fields for user to enter data
+year = st.number_input("Year", min_value=2000, max_value=2025, value=2020)
 engine_size = st.number_input("Engine Size", min_value=0.5, max_value=10.0, value=2.5)
-mileage = st.number_input("Mileage", min_value=0, max_value=2000000, value=15000)
-type = st.selectbox("Car Type", type_options)
-make_ = st.selectbox("Car make", make_options)
-
-
-
-# menu for car tpe
-
-
-#car option
-options_options = df['Options'].unique().tolist()
+mileage = st.number_input("Mileage", min_value=0, max_value=200000, value=15000)
+type_options = ["Accent", "LandCruiser"]
+type_ = st.selectbox("Car Type", type_options)
+make_options = ["Hyundai", "Mercedes",""]
+make = st.selectbox("Car Make", make_options)
+options_options = ["Full", "Standard"]
 options = st.selectbox("Car Options", options_options)
 
-# submit
+# Button to submit the input data
 if st.button("Predict Cluster"):
     # Prepare the data to send to the FastAPI endpoint
     input_data = {
         "Year": year,
         "Engine_Size": engine_size,
         "Mileage": mileage,
-        "Type": type,
-        "Make": make_,
+        "Type": type_,
+        "Make": make,
         "Options": options
     }
 
-    # Send POST
+    # Send POST request to the deployed FastAPI server
     try:
         response = requests.post("https://usecase-4-api.onrender.com/predict", json=input_data)
 
-        # predict
+        # Get the prediction result from the response
         if response.status_code == 200:
             prediction = response.json().get("pred")
             st.success(f"Predicted Cluster: {prediction}")
